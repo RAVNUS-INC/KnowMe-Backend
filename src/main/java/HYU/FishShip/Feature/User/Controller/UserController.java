883984afdfca7fef38c12693fb884e14e5c29545
LeeHyunWoo02 <hyoun0824@naver.com>
2025-05-25 20:43:48 +0900

    feat : 회원정보 수정 기능

---
+++ src/main/java/HYU/FishShip/Feature/User/Controller/UserController.java
@@ -0,0 +1,45 @@
+package HYU.FishShip.Feature.User.Controller;
+
+import HYU.FishShip.Feature.User.Dto.UserEditRequestDTO;
+import HYU.FishShip.Feature.User.Dto.UserEditResponseDTO;
+import HYU.FishShip.Feature.User.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Operation(summary = "회원정보 수정", description = "회원 정보를 받아 수정합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원정보 수정 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "500", description = "서버 오류")
+    })
+    @PostMapping("/edit/{userId}")
+    private ResponseEntity<UserEditResponseDTO<Long>> editUser(@PathVariable Long userId,
+                                                               @RequestBody UserEditRequestDTO requestDTO) {
+
+        try {
+            userService.editUser(userId ,requestDTO);
+            return ResponseEntity.ok(new UserEditResponseDTO<>(HttpStatus.OK, "회원정보 수정 성공",
+                    userId));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new UserEditResponseDTO<>(HttpStatus.BAD_REQUEST,
+                    "회원정보 수정 도중 오류가 발생했습니다.", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    new UserEditResponseDTO<>(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류", null));
+        }
+
+    }
+}
--- src/main/java/HYU/FishShip/Feature/User/Service/UserService.java
+++ src/main/java/HYU/FishShip/Feature/User/Service/UserService.java
@@ -1,4 +1,49 @@
 package HYU.FishShip.Feature.User.Service;
 
+import HYU.FishShip.Core.Entity.User;
+import HYU.FishShip.Core.Repository.UserRepository;
+import HYU.FishShip.Feature.User.Dto.UserEditRequestDTO;
+import jakarta.transaction.Transactional;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository,@Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public void editUser(Long userId, UserEditRequestDTO userEditRequestDTO) {
+        String email = userEditRequestDTO.getEmail();
+        String phone = userEditRequestDTO.getPhone();
+        String newPassword = userEditRequestDTO.getPassword();
+
+        try {
+            Optional<User> updatedUser = userRepository.findById(userId);
+            if (updatedUser.isPresent()) {
+                Optional.ofNullable(email).ifPresent(updatedUser.get()::setEmail);
+                Optional.ofNullable(phone).ifPresent(updatedUser.get()::setPhone);
+                if (newPassword != null) {
+                    String password = updatedUser.get().getPassword();
+                    if (!passwordEncoder.matches(newPassword, password)) {
+                        updatedUser.get().setPassword(passwordEncoder.encode(newPassword));
+                    } else {
+                        throw new IllegalArgumentException("비밀번호가 같습니다. ");
+                    }
+                }
+                userRepository.save(updatedUser.orElse(null));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("사용자 정보 수정 중 오류가 발생했습니다: " + e.getMessage(), e);
+        }
+    }
 }
